4b73bb23e1b2a9dd957e899a8ff861bb5e60f44d
raniless <raniless@naver.com>
2020-11-19 00:29:04 +0900

    학습 테스트 추가

--- src/test/java/springbook/learningtest/spring/ioc/bean/ContextTest.java
+++ src/test/java/springbook/learningtest/spring/ioc/bean/ContextTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -55,5 +56,25 @@ public class ContextTest {
         hello.print();
 
         assertEquals("Hello Spring", context.getBean("printer").toString());
+    }
+
+    @Test
+    public void parentChildContextTest() {
+        ApplicationContext parent = new GenericXmlApplicationContext("parentContext.xml");
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions("childContext.xml");
+        child.refresh();
+
+        //child에 없는 경우 parent에서 검색하여 있으면 가져온다.
+        Printer printer = child.getBean("printer", Printer.class);
+        assertNotNull(printer);
+
+        //parent에 빈이 있더라도 child에 있으면 child 빈을 가져온다.
+        Hello hello = child.getBean("hello", Hello.class);
+        assertNotNull(hello);
+
+        hello.print();
+        assertEquals("Hello Child", printer.toString());
     }
 }
