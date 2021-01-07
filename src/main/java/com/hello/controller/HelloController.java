7e98e7da8cf4db64bb5b7ca568c6123d71d328e0
raniless <raniless@naver.com>
2021-01-08 00:29:49 +0900

    ExcelView 학습 및 라이브러리 추가

--- src/main/java/com/hello/controller/HelloController.java
+++ src/main/java/com/hello/controller/HelloController.java
@@ -2,16 +2,25 @@ package com.hello.controller;
 
 import com.hello.entity.User;
 import com.hello.view.HelloPdfView;
+import com.hello.view.HelloXlsView;
+import com.hello.view.HelloXlsxView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
     @Autowired
     private HelloPdfView helloPdfView;
+    @Autowired
+    private HelloXlsView helloXlsView;
+    @Autowired
+    private HelloXlsxView helloXlsxView;
 
     @RequestMapping("/hello")
     public String hello(ModelMap model) {
@@ -31,5 +40,27 @@ public class HelloController {
     public ModelAndView registerPdf(ModelMap model) {
         model.put("message", "Gooooooooooooooooooooooooooooooooooooooood!!");
         return new ModelAndView(helloPdfView, model);
+    }
+
+    @RequestMapping("/helloxls")
+    public ModelAndView registerXls(ModelMap model) {
+        List<String> list = new ArrayList<>();
+        list.add("캡슐화");
+        list.add("상속성");
+        list.add("다형성");
+
+        model.put("oop", list);
+        return new ModelAndView(helloXlsView, model);
+    }
+
+    @RequestMapping("/helloxlsx")
+    public ModelAndView registerXlsx(ModelMap model) {
+        List<String> list = new ArrayList<>();
+        list.add("캡슐화");
+        list.add("상속성");
+        list.add("다형성");
+
+        model.put("oop", list);
+        return new ModelAndView(helloXlsxView, model);
     }
 }
---
+++ src/main/java/com/hello/view/HelloXlsxView.java
@@ -0,0 +1,42 @@
+package com.hello.view;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+public class HelloXlsxView extends AbstractXlsxView {
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        //데이터 가져오기
+        List<String> list = (List<String>)model.get("oop");
+
+        //시트 만들기
+        Sheet sheet = workbook.createSheet();
+
+        //시트이름 만들기
+        workbook.setSheetName(0, "객체 지향");
+
+        //컬럼 너비 설정
+        sheet.setColumnWidth(0, 256*20);
+
+        //제목 셀 만들기
+        Row firstRow = sheet.createRow(0);
+        Cell firstCell = firstRow.createCell(0);
+        firstCell.setCellValue("객체 지향 언어의 3대 특징");
+
+        //데이터 출력
+        int rowIdx = 1;
+        for(String str : list) {
+            Row row = sheet.createRow(rowIdx++);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(str);
+        }
+    }
+}
